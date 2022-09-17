35056860f22cd3e8d397a21e5d8fda00bb6447ee
phanumant228 <phanumant228@gmail.com>
2022-09-17 19:34:03 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -23,6 +23,7 @@ class App extends React.Component {
       <div id="main">
           <div id="count">{this.state.count}</div>
           <button id="incr" onClick={this.handleClick}>Increment</button>
+          console.log("moumted")
       </div>
     )
   }
